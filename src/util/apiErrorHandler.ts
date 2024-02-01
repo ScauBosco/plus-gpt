73794d76a14366978a374d806958ed0902f60704
黄嘉诚 <huangyiqi@huanglaobandeMacBook-Pro.local>
2024-02-02 03:10:09 +0800

    fix 60

--- src/util/apiErrorHandler.ts
+++ src/util/apiErrorHandler.ts
@@ -1,32 +1,31 @@
-import {NextApiRequest, NextApiResponse} from 'next';
+import { NextApiRequest, NextApiResponse } from "next";
 
-export const maxDuration = 60
-export const apiErrorHandler = (fn: any) => async (req: NextApiRequest, res: NextApiResponse) => {
-
-    try {
-        const data = await fn(req, res);
-        if (typeof data === 'string') {
-            // 直接返回字符串
-            res.status(200).send(data);
-        }
-        else {
-            res.status(200).json({
-                data,
-            });
-        }
-    }
-    catch (error: any) {
-        const statusCode = 200;
-        const message = error.message || 'Internal Server Error';
-        console.error(error);
-        res.status(statusCode).json({
-            errors: [
-                {
-                    message,
-                    code: error.code || 3,
-                },
-            ]
-            ,
-        });
-    }
+export const config = {
+  maxDuration: 60,
 };
+export const apiErrorHandler =
+  (fn: any) => async (req: NextApiRequest, res: NextApiResponse) => {
+    try {
+      const data = await fn(req, res);
+      if (typeof data === "string") {
+        // 直接返回字符串
+        res.status(200).send(data);
+      } else {
+        res.status(200).json({
+          data,
+        });
+      }
+    } catch (error: any) {
+      const statusCode = 200;
+      const message = error.message || "Internal Server Error";
+      console.error(error);
+      res.status(statusCode).json({
+        errors: [
+          {
+            message,
+            code: error.code || 3,
+          },
+        ],
+      });
+    }
+  };
