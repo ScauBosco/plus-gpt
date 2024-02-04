8730c5845f209adc677058cce854c0098f26a2e5
huangjiacheng01 <huangjiacheng01@MacBook-Pro-2.local>
2024-02-04 23:55:08 +0800

    asd

--- src/pages/api/chat-sse.ts
+++ src/pages/api/chat-sse.ts
@@ -1,5 +1,4 @@
 import { APIKEY, BASEURL } from "@/constant/config";
-import { apiErrorHandler } from "@/util/apiErrorHandler";
 import { NextApiRequest, NextApiResponse } from "next";
 import OpenAI from "openai";
 
@@ -7,19 +6,24 @@ const openai = new OpenAI({
   apiKey: APIKEY,
   baseURL: BASEURL,
 });
+const DEFAULT_PROMPT = "生成一个苹果";
 
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   // 设置必要的 HTTP 头
   res.setHeader("Content-Type", "text/event-stream");
   res.setHeader("Cache-Control", "no-cache");
   res.setHeader("Connection", "keep-alive");
   res.flushHeaders();
 
-  let prompt = "";
+  let prompt = DEFAULT_PROMPT;
   if (req.method === "POST") {
     // 解析POST请求体
-    const data = req.body;
-    prompt = data.prompt;
+    const data = JSON.parse(req.body);
+    console.log("req.body", data.prompt);
+    if (data.prompt) prompt = data.prompt;
   }
 
   // 处理关闭连接
@@ -27,19 +31,32 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     res.end();
   });
 
-  // 发起请求到 OpenAI 的接口
-  const response = await openai.chat.completions.create({
-    temperature: 0.01,
-    model: "gpt-3.5-turbo",
-    messages: [
-      { role: "system", content: "You are a helpful assistant." },
-      { role: "user", content: prompt },
-    ],
-  });
-
-  // 将数据流式地发送到客户端
-  res.write(`data: ${JSON.stringify(response)}\n\n`);
+  try {
+    // 发起请求到 OpenAI 的接口
+    const response = await openai.chat.completions.create({
+      temperature: 0.01,
+      model: "gpt-3.5-turbo",
+      messages: [
+        { role: "system", content: "You are a helpful assistant." },
+        { role: "user", content: JSON.stringify({ prompt }) },
+      ],
+      stream: true,
+    });
+    // 将数据流式地发送到客户端
+    for await (const chunk of response) {
+      let message = chunk.choices[0]?.delta?.content || "";
+      console.log(message)
+      res.write(`data: ${JSON.stringify(message)}\n\n`);
+    }
+    // res.write(`data: ${JSON.stringify(response)}\n\n`);
+    // res.write(`data: ${JSON.stringify(response)}\n\n`);
+    // res.write(`data: ${JSON.stringify(response)}\n\n`);
+    // res.write(JSON.stringify({response}));
+  } catch (e) {
+    res.write(e);
+    res.end();
+    throw e;
+  }
 
   res.end();
-};
-export default apiErrorHandler(handler);
+}
